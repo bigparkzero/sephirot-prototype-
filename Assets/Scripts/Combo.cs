6d45c0453235b08a494fd849da455a0866ade7c2
Hoz94 <zoh9401@gmail.com>
2024-07-03 14:33:40 +0900

    무기교체 시스템 정리
    검과 도끼 교체 만들기
    교체시 모션 구현

--- Assets/Scripts/Combo.cs
+++ Assets/Scripts/Combo.cs
@@ -11,13 +11,32 @@ public class Combo : MonoBehaviour
     }
     private void Update()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             an.SetTrigger("attack");
 
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            an.SetInteger("weaponID", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            an.SetInteger("weaponID", 1);
+        }
+    }
 
-       
+    public void changeWeapon(int weaponID)
+    {
+        switch (weaponID)
+        {
+            case 0:
+                //Ä®
+                break;
+            case 1:
+                //µµ³¢
+                break;
+        }
     }
 }
