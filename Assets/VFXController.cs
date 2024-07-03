22ad4266afd530790d9840e7c894dda30a8a16d9
Hoz94 <zoh9401@gmail.com>
2024-07-03 12:08:30 +0900

    12

--- Assets/VFXController.cs
+++ Assets/VFXController.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
@@ -20,5 +21,58 @@ public class VFXController : MonoBehaviour
     {
         visualEffect[a].Stop();
     }
-    
+   
+}
+*/
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXController : MonoBehaviour
+{
+    public VisualEffect visualEffect;
+
+    void Start()
+    {
+        if (visualEffect == null)
+        {
+            visualEffect = GetComponent<VisualEffect>();
+        }
+    }
+
+    // 애니메이션 이벤트를 통해 호출되는 함수
+    public void PlayVFX(string jsonParams)
+    {
+        if (!string.IsNullOrEmpty(jsonParams))
+        {
+            VFXParams parameters = JsonUtility.FromJson<VFXParams>(jsonParams);
+            Vector3 position = new Vector3(parameters.positionX, parameters.positionY, parameters.positionZ);
+            Vector3 rotation = new Vector3(parameters.rotationX, parameters.rotationY, parameters.rotationZ);
+
+            // 이펙트의 위치와 회전을 설정
+            visualEffect.transform.position = position + transform.position;
+            visualEffect.transform.eulerAngles = rotation + transform.eulerAngles;
+            visualEffect.Play();
+        }
+    }
+
+    // 데이터 직렬화 구조체
+    [System.Serializable]
+    public struct VFXParams
+    {
+        public float positionX;
+        public float positionY;
+        public float positionZ;
+        public float rotationX;
+        public float rotationY;
+        public float rotationZ;
+    }
+
+    // 이펙트를 중지하는 함수
+    public void StopVFX()
+    {
+        if (visualEffect != null)
+        {
+            visualEffect.Stop();
+        }
+    }
 }
